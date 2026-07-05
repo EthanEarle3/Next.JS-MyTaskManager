f3dcdfcdfaee2e143aba896e017b8a8a17212528
Ethan Earle <earle121@usf.edu>
2026-07-05 16:43:57 -0400

    Tasklist notes

--- task-manager/src/app/components/Tasklist.js
+++ task-manager/src/app/components/Tasklist.js
@@ -1,3 +1,8 @@
+// Component: TaskList
+// This component is responsible for rendering a list of tasks using the TaskCard component
+// It receives an array of tasks and two callback functions as props: onToggle and onDelete
+// The onToggle function is called when a task's completion status is toggled, and the onDelete function is called when a task is deleted
+// The component filters out any malformed tasks to ensure that only valid task objects are rendered
 import TaskCard from "./Taskcard";
 
 export default function TaskList({ tasks = [], onToggle, onDelete }) {
@@ -13,7 +18,8 @@ export default function TaskList({ tasks = [], onToggle, onDelete }) {
       </div>
     );
   }
-
+// Renders a list of components using key={task.id} to ensure that each component has a unique identifier for efficient rendering and updating
+// The TaskCard component is used to display each individual task, and it receives the task object, onToggle function, and onDelete function as props
   return (
     <ul className="divide-y divide-slate-50 border border-slate-100 rounded-xl overflow-hidden">
       {cleanTasks.map((task) => (
